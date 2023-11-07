cb2cfa1e085031224cf923907b4ad3f02cf99ca3
sindhu962 <145537788+sindhu962@users.noreply.github.com>
2023-11-07 18:03:48 +0530

    Add files via upload

--- one/index.js
+++ one/index.js
@@ -19,3 +19,19 @@ for (var i = 0; i < odd.length; i++)
      odd[i].style.backgroundColor='#f4f4f4';
 even[i].style.backgroundColor='#ccc';
 }
+var newDiv = document.createElement('div');
+// Add class
+ newDiv.className= 'hello';
+// Add id
+ newDiv.id = 'hello1';
+// Add attr 
+newDiv.setAttribute('title', 'Hello Div');
+// Create text node
+ var newDivText = document.createTextNode('Hello World');
+// Add text to div 
+newDiv.appendChild(newDivText);
+var container = document.querySelector('header .container');
+var h1 =document.querySelector("header h1");
+console.log(newDiv);
+newDiv.style.fontSize = '30px'
+container.insertBefore(newDiv, h1);
